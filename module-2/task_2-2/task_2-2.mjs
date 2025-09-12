2254d0f2eee6b9903f84f309e0613b7965cb64be
shirin venedik <shirinva@uia.no>
2025-09-12 18:27:16 +0200

    ferdig m task 2.2

--- module-2/task_2-2/task_2-2.mjs
+++ module-2/task_2-2/task_2-2.mjs
@@ -22,11 +22,11 @@ printOut("--- Part 3 -----------------------------------------------------------
 const part3Days = 3, part3Hours = 14, part3Minutes = 14, part3Seconds = 34;
 let part3Answer =
 (part3Days * 24 * 60) +
-(part3Hours * 60) + 
+(part3Hours * 60 ) + 
 part3Minutes +
-(part3Seconds / 60)
+(part3Seconds / 60);
 part3Answer = part3Answer.toFixed(2);
-printOut("3 days, 12 hours, 14 minutes, and 45 second is"); + part3Answer + "minutes"
+printOut("3 days, 12 hours, 14 minutes, and 45 second is"+ part3Answer + "minutes");
 printOut(newLine);
 
 printOut("--- Part 4 ----------------------------------------------------------------------------------------------");
@@ -40,42 +40,89 @@ const part4Hours =Math.floor(part4Rest);
 part4Rest = part4Rest - part4Hours;
 part4Rest = part4Rest * 60;
 const part4Minute = Math.floor(part4Rest);
+part4Rest -= part4Minute;
+part4Rest *= 60;
+const part4Seconds = Math.floor (part4Rest);
 
 printOut("6,322.52 minutes is" + 
 part4Days + " days, " +
-part4Hours + " hours, " +
-part4Minute + "minutes"
+part4Hours + " hours, " + 
+part4Minute + " minutes, " +
+part4Seconds + " seconds"
 );
 
 printOut(newLine);
 
 printOut("--- Part 5 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Convert 54 dollars to Norwegian kroner */
+const usd = 54;
+const nok = 54;
+
+const usdToNokRate = 76 / 8.6;
+const nokToUsdRate = 8.6 / 76;
+
+let usdToNok = Math.round(usd * usdToNokRate);
+let nokToUsd = Math.round(nok * nokToUsdRate);
+
+
+printOut(`54 USD → ${usdToNok} NOK`);
+printOut(`54 NOK → ${nokToUsd} USD`);
+
+(printOut);
 printOut(newLine);
 
+
 printOut("--- Part 6 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* There is much between heaven and earth that we do not understand. */
+const text = "There is much between heaven and earth that we do not understand.";
+printOut(66);
+printOut("h");
+printOut("earth th");
+printOut(31);
+
+printOut();
 printOut(newLine);
 
 printOut("--- Part 7 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Comparison, print the values for the following expressions (evaluate whether the statements are true):*/
+printOut(String(5 > 3));
+printOut(String(7 >= 7));
+printOut(String("a" > "b"));
+printOut(String("1" < "a"));
+printOut(String("2500" < "abcd"));
+printOut(String("arne" != "thomas"));
+printOut(String(2 == 5));
+printOut(String("abcd" > "bcd"));
+
+printOut();
 printOut(newLine);
 
 printOut("--- Part 8 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Convert and print the following expressions:*/
+
+printOut(Number("254"));        
+printOut(Number("57.23"));      
+printOut(Number("25 kroner"));
+
+
+printOut();
 printOut(newLine);
 
 printOut("--- Part 9 ----------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Create a variable "r" and randomly generate a number from 1 to 360 (1 >= r <= 360).*/
+let r = Math.floor(Math.random() * 360) + 1;
+printOut(r);
+
 printOut(newLine);
 
 /* Task 10*/
 printOut("--- Part 10 ---------------------------------------------------------------------------------------------");
-/* Put your code below here!*/
-printOut("Replace this with you answer!");
+/* Use modulus (%) to calculate how many weeks and days are in 131 days.*/
+let days = 131;
+let weeks = Math.floor(days / 7);
+let restDays = days % 7;
+printOut("Weeks:" + weeks);
+printOut("Days:" + restDays);
+
+printOut();
 printOut(newLine);
